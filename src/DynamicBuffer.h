397fdeb1bc6e732a37d65083a74ab3a465df9e6d
Will Miles <will@willmiles.net>
2024-08-05 11:09:36 -0400

    AbstractWebResponse: Replace std::vector
    Add a "Walkable" wrapper for DynamicBuffer to support partial buffer
    reads.  This permits the code to be extended to handle allocation
    failures, where std::vector gave us no option but to crash.
    
    Of course this code still makes no attempt to actually handle an
    allocation failure.

--- src/DynamicBuffer.h
+++ src/DynamicBuffer.h
@@ -153,3 +153,85 @@ class BufferListPrint : public Print {
 typedef BufferListPrint<DynamicBufferList> DynamicBufferListPrint;
 typedef BufferListPrint<SharedBufferList> SharedBufferListPrint;
 
+
+// Walkable buffer
+// A buffer class that permits "consuming" data from either end, adjusting data() and size() to match
+template<typename buffer_type>
+class Walkable
+{
+  buffer_type _buf;
+  size_t _left, _right;
+
+  public:
+  Walkable() : _left(0), _right(0) {};
+  explicit Walkable(size_t len) : _buf(len), _left(0), _right(0) {};
+  Walkable(const char* buf, size_t len) : _buf(buf, len), _left(0), _right(0) {};
+  explicit Walkable(const String& s) : _buf(s), _left(0), _right(0) {};
+  explicit Walkable(String&& s) : _buf(std::move(s)), _left(0), _right(0) {};
+
+  // Accessors
+  // Buffer interface
+  char* data() const { return _buf.data() + _left; };
+  size_t size() const { return _buf.size() - (_left + _right); };
+  size_t capacity() const { return _buf.size(); };  // for similarity with STL types
+  explicit operator bool() const { return (buffer_type::data() != nullptr) && (size() > 0); }
+  char& operator[](ptrdiff_t p) const { return *(data() + p); };
+  void clear() { _buf.clear(); reset(); };
+
+  // Raw interface
+  const buffer_type& buffer() const { return _buf; };
+  size_t offset() const { return _left; }
+  size_t roffset() const { return _right; }  
+  
+  // Modifiers
+  void reset() { _left = _right = 0; }; // Reset the walking counters
+  void advance(ptrdiff_t count) { // Consume some data from the left hand side
+    if (count > 0) {
+      _left = std::min(_left+count, _buf.size() - _right);
+    } else {
+      if (abs(count) <= _left) {
+        _left += count;
+      } else {
+        _left = 0;  // do not wrap
+      }
+    }
+  }
+  void radvance(ptrdiff_t count) {  // Consume some data from the right hand side
+    if (count > 0) {
+      _right = std::min(_right+count, _buf.size() - _left);
+    } else {
+      if (abs(count) <= _right) {
+        _right += count;
+      } else {
+        _right = 0;  // do not wrap
+      }
+    }
+  }
+
+  // Contract buffer to specified size
+  size_t resize(size_t s) {
+    auto bs = _buf.size() - _left;    
+    _right = s <= bs ? (bs - s) : 0U;
+    return size();
+  }
+
+  // Resize the underlying buffer storage, preserving contents
+  // Returns new size on success, current size on failure.
+  size_t realloc(size_t s) {
+    if (s <= size()) {
+      auto new_buf = buffer_type(data(), s);
+      if (new_buf) {
+        _buf = std::move(new_buf);
+        reset();
+      }
+    } else {
+      auto new_buf = buffer_type(s);
+      if (new_buf) {
+        memcpy(new_buf.data(), data(), size());
+        _buf = std::move(new_buf);
+        reset();
+      }
+    }
+    return _buf.size();
+  }
+};
--- src/WebResponseImpl.h
+++ src/WebResponseImpl.h
@@ -26,8 +26,7 @@
 #undef min
 #undef max
 #endif
-#include <vector>
-#include "default_init_allocator.h"
+#include "DynamicBuffer.h"
 // It is possible to restore these defines, but one can use _min and _max instead. Or std::min, std::max.
 
 class AsyncBasicResponse: public AsyncWebServerResponse {
@@ -47,7 +46,7 @@ class AsyncAbstractResponse: public AsyncWebServerResponse {
     // This is inefficient with vector, but if we use some other container, 
     // we won't be able to access it as contiguous array of bytes when reading from it,
     // so by gaining performance in one place, we'll lose it in another.
-    std::vector<uint8_t, default_init_allocator<uint8_t>> _packet, _cache;
+    Walkable<DynamicBuffer> _packet, _cache;
     size_t _readDataFromCacheOrContent(uint8_t* data, const size_t len);
     size_t _fillBufferAndProcessTemplates(uint8_t* buf, size_t maxLen);
   protected:
